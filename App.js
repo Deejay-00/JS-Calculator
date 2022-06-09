2192effdb29bc21ad81961bbeac54c6766ce61b3
Jamiu <jammy190@gmail.com>
2022-06-09 10:27:06 +0100

    resolves equal button issue

--- App.js
+++ App.js
@@ -3,25 +3,28 @@
     let screen = document.querySelector('.screen');
     let buttons = document.querySelectorAll('.btn');
     let clear = document.querySelector('.btn-clear');
-    let equal = document.querySelector('.btn-equal');
+    let equal = document.getElementById('equal');
+
 
     buttons.forEach(function (button) {
-        button.addEventListener('click', function (e) {
-            let value = e.target.dataset.num;  
-            screen.value += value;
-        })
+        if (button != equal) {
+            button.addEventListener('click', function (e) {
+                let value = e.target.dataset.num;
+                screen.value += value;
+            })
+        }
     });
 
     equal.addEventListener('click', function (e) {
-        if (screen.value === '') {
+        if (screen.value === "") {
             screen.value = "";
         } else {
             let answer = eval(screen.value);
-            screen.value =+ answer;
+            screen.value = answer;
         }
     })
 
-    clear.addEventListener('click', function(e){
+    clear.addEventListener('click', function (e) {
         screen.value = "";
     })
 
