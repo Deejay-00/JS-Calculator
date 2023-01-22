a9b09116ce681f21c0619ee24726858dff5f1fb3
Dare Jamiu <jammy190@gmail.com>
2023-01-22 08:50:38 +0100

    changes

--- App.js
+++ App.js
@@ -18,10 +18,12 @@
     equal.addEventListener('click', function (e) {
         if (screen.value === "") {
             screen.value = "";
-        } else {
+        }
+
+        else {
             let answer = eval(screen.value);
             screen.value = answer;
-        }
+        }      
     })
 
     clear.addEventListener('click', function (e) {
